6c39fe7e513a7acee48d2e8b4aeb55f4904f61d4
Amina-Dirie <adirie522@gmail.com>
2023-07-30 19:01:33 +0100

    feat: fetching the name from the API

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, { useEffect, useState } from 'react';
+
+const App: React.FC = () => {
+  const [person, setPerson] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/people').then((response) => {
+      
+        return response.json();
+      })
+      .then((data) => {
+        setPerson(data.results[0].name);
+      })
+     
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>{person}</h1>
     </div>
   );
-}
+};
 
 export default App;
+
+
